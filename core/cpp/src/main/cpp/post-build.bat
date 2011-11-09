7e3f9f3079b2d3db1606f2d577ffc5a7bba55042
Gili Tzabari <cowwoc@bbs.darktech.org>
2011-11-08 20:39:27 -0500

    Exclude unwanted dependencies in Maven instead of post-build.bat

--- core/cpp/src/main/cpp/post-build.bat
+++ core/cpp/src/main/cpp/post-build.bat
@@ -3,15 +3,7 @@ rem ********************************
 rem * %1 = build configuration     *
 rem * %2 = boost library directory *
 rem * %3 = output directory        *
-rem * %4 = visual studio version   *
 rem ********************************
 
-if /i %1 == "debug" (
-  echo Copying debug dependencies to output directory...
-  copy "%2\*boost_thread-vc%4-mt-gd-1*" "%3\%1" /y
-  copy "%2\*boost_date_time-vc%4-mt-gd-1*" "%3\%1" /y
-) else (
-  echo Copying release dependencies to output directory...
-  copy "%2\*boost_thread-vc%4-mt-1*" "%3\%1" /y
-  copy "%2\*boost_date_time-vc%4-mt-1*" "%3\%1" /y
-)
+echo Copying dependencies to output directory...
+copy "%2\*" "%3\%1" /y
