fb491077defc383e19f4c9011e84073118497f7c
Gili Tzabari <cowwoc@bbs.darktech.org>
2013-07-22 00:57:55 -0400

    - Strip quotes from post-build.bat parameters. - Use C/CXX flags when building examples.

--- examples/peer_example1/cpp/src/main/cpp/post-build.bat
+++ examples/peer_example1/cpp/src/main/cpp/post-build.bat
@@ -6,6 +6,6 @@ rem * %3 = output directory    *
 rem ****************************
 
 echo Copying dependencies to output directory...
-copy /b "%2\lib\*" "%3\%1" /y
-copy /b "%3\..\..\..\..\java\target\jace-examples-peer-example1*.jar" "%3\%1\peer_example1.jar" /y
-copy /b "%2\jace-core-runtime-*" "%3\%1\" /y
+copy /b "%~2\lib\*" "%~3\%~1" /y
+copy /b "%~3\..\..\..\..\java\target\jace-examples-peer-example1*.jar" "%~3\%~1\peer_example1.jar" /y
+copy /b "%~2\jace-core-runtime-*" "%~3\%~1\" /y
--- examples/perf_test/src/main/cpp/post-build.bat
+++ examples/perf_test/src/main/cpp/post-build.bat
@@ -6,5 +6,5 @@ rem * %3 = output directory    *
 rem ****************************
 
 echo Copying dependencies to output directory...
-copy /b "%2\lib\*" "%3\%1" /y
-copy /b "%2\jace-core-runtime-*" "%3\%1\jace-runtime.jar" /y
+copy /b "%~2\lib\*" "%~3\%~1" /y
+copy /b "%~2\jace-core-runtime-*" "%~3\%~1\jace-runtime.jar" /y
