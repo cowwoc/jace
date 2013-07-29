e8499287da668c7be709a8a5c1793be5eca0d714
Gili Tzabari <cowwoc@bbs.darktech.org>
2013-07-29 00:19:23 -0400

    - post-build.sh needed to copy jace/boost libraries from lib/*, not /*. - Replaced LINK_DIRECTORIES with TARGET_LINK_LIBRARIES.

--- examples/peer_example1/cpp/src/main/cpp/post-build.sh
+++ examples/peer_example1/cpp/src/main/cpp/post-build.sh
@@ -6,6 +6,6 @@
 #############################
 
 echo "Copying dependencies to output directory..."
-cp "$1/"*.so "$2/"
+cp "$1/lib/"*.so "$2/"
 cp "$1/../../../../java/target/"jace-examples-peer-example1*.jar "$2/peer_example1.jar"
 cp "$1/"jace-core-runtime-*.jar "$2/jace-runtime.jar"
--- examples/singleton/cpp/src/main/cpp/post-build.sh
+++ examples/singleton/cpp/src/main/cpp/post-build.sh
@@ -6,6 +6,6 @@
 #############################
 
 echo "Copying dependencies to output directory..."
-cp "$1/"*.so "$2/"
+cp "$1/lib/"*.so "$2/"
 cp "$1/../../../../java/target/"jace-examples-singleton*.jar "$2/singleton.jar"
 cp "$1/"jace-core-runtime-*.jar "$2/jace-runtime.jar"
